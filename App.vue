4d099c42b656198164112beda1dfa70e45ebc68c
Mateusz Wołowicz <48186265+ledinos1@users.noreply.github.com>
2020-09-06 16:00:13 +0200

    new

---
+++ App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Nav/>
+    <FlashMessage/>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Nav from "@/components/Nav.vue";
+import Vue from 'vue';
+import FlashMessage from '@smartweb/vue-flash-message';
+Vue.use(FlashMessage);
+export default {
+  name: "Main",
+  components: {
+    Nav,
+    FlashMessage
+  }
+};
+</script>
+<style>
+body{
+  background-image: url('https://bitdefender.pl/wp-content/uploads/2018/09/features-background-ultra.jpg');
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+    <b-navbar type="dark" variant="dark">
+        <b-navbar-nav>
+        <b-nav-item href="/">Home</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Lang" left>
+            <b-dropdown-item href="/about">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="User" left>
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+        </b-nav-item-dropdown>
+        </b-navbar-nav>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
